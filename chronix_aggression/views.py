87e1ad859a209c64bd6ba94a43c70ed13549ff79
Pawlo <pawlonazaruk@gmail.com>
2020-12-20 04:43:09 +0100

    Index view template extended
    The view function reads all songs from database and transfer it to template, where every song is displayed.

--- chronix_aggression/views.py
+++ chronix_aggression/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Song
+from pprint import pprint
 # Create your views here.
 
 def index(request):
-    return render(request, "chronix_aggression/index.html")
+    songs = Song.objects.all()
+    context = {
+        "songs": songs,
+        "temp": "asd",
+    }
+    pprint(context)
+    return render(request, "chronix_aggression/index.html", context)
